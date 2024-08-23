3dbc236c2d2d6e665115fb4780746680c5f9364f
Cross <github@dillfrescott.com>
2024-08-22 23:59:14 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,7 @@ export default function RootLayout({
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <script defer src="https://medama.dill.moe/script.js"></script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
